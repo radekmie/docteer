c2c39cec4296fdd761ce4d23c2fbe375f13ee714
Radosław Miernik <radekmie@gmail.com>
2017-08-13 13:12:36 +0200

    Inlined CSS.

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ import cssnano         from 'cssnano';
 import postcss         from 'postcss';
 import {minify}        from 'html-minifier';
 import {prepackString} from 'prepack/lib/prepack-standalone';
+import {readFile}      from 'fs';
 
-import {Accounts}    from 'meteor/accounts-base';
-import {Boilerplate} from 'meteor/boilerplate-generator';
-import {Meteor}      from 'meteor/meteor';
-import {check}       from 'meteor/check';
+import {Accounts}        from 'meteor/accounts-base';
+import {Boilerplate}     from 'meteor/boilerplate-generator';
+import {Meteor}          from 'meteor/meteor';
+import {WebAppInternals} from 'meteor/webapp';
+import {check}           from 'meteor/check';
 
 import {Proofs} from '/imports/api/proofs';
 
@@ -16,32 +18,33 @@ if (Meteor.users.find({}, {fields: {_id: 1}, limit: 1}).count() === 0)
     Accounts.createUser({email: 'admin@doctear.com', password: 'doctear'});
 
 if (process.env.NODE_ENV === 'production') {
-    const config = require('../package.json');
+    const bundledCSSPath = Object.values(WebAppInternals.staticFiles).find(file => file.type === 'css').absolutePath;
+    const bundledCSS = Meteor.wrapAsync(readFile)(bundledCSSPath, 'utf8');
 
     const minifyCSSLib = postcss([
-        cssnano(config.postcss.plugins.cssnano),
-        autoprefixer(config.postcss.plugins.autoprefixer)
+        autoprefixer({browsers: ['last 2 versions']}),
+        cssnano({preset: 'default'})
     ]);
 
-    const minifyCSSRaw = css => minifyCSSLib.process(css).then().await().css;
+    const minifyCSSRaw = css => minifyCSSLib.process(css + bundledCSS).then().await().css;
     const minifyCSSCache = {};
     const minifyCSS = css => minifyCSSCache[css] || (minifyCSSCache[css] = minifyCSSRaw(css));
 
     const minifyJSRaw = js => UglifyJS.minify(prepackString('unknown', js).code).code
-        .replace(/meteorRelease:"METEOR@(.*?)",/, '')
         .replace(',TEST_METADATA:"{}"', '')
         .replace(',autoupdateVersionCordova:"none"', '')
+        .replace(/meteorRelease:"METEOR@(.*?)",/, '')
     ;
 
     const minifyJSCache = {};
     const minifyJS = js => minifyJSCache[js] || (minifyJSCache[js] = minifyJSRaw(js));
 
-    const options = {collapseWhitespace: true, minifyCSS, minifyJS};
-
+    const minifyHTMLOptions = {collapseWhitespace: true, minifyCSS, minifyJS};
     const minifyHTMLRaw = html =>
-        minify(html, options)
+        minify(html, minifyHTMLOptions)
             .replace('<html>', '<html lang="en">')
-            .replace(/rel="stylesheet"/g, 'as="style" onload="this.rel=\'stylesheet\'" rel="preload"')
+            .replace('?meteor_js_resource=true', '')
+            .replace(/<link .*?>/, '')
     ;
 
     const minifyHTMLCache = {};
