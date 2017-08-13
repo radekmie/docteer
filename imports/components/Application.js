0e909f11e3655a5a2a23ee4d15d7914d66c9f2da
Radosław Miernik <radekmie@gmail.com>
2017-08-13 19:27:32 +0200

    Better UX for action buttons.

--- imports/components/Application.js
+++ imports/components/Application.js
@@ -14,11 +14,11 @@ import {
 } from '/imports/state/actions';
 
 /* eslint-disable max-len */
-const iconAdd = icon('M417.4 224H288V94.6c0-16.9-14.3-30.6-32-30.6s-32 13.7-32 30.6V224H94.6C77.7 224 64 238.3 64 256s13.7 32 30.6 32H224v129.4c0 16.9 14.3 30.6 32 30.6s32-13.7 32-30.6V288h129.4c16.9 0 30.6-14.3 30.6-32s-13.7-32-30.6-32z');
-const iconDo = icon('M448 294.4v-76.8h-42.8c-3.4-14.4-8.9-28-16.1-40.5l29.8-29.7-54.3-54.3-29.1 29.1c-12.6-7.7-26.4-13.5-41.1-17.3V64h-76.8v40.9c-14.7 3.8-28.5 9.7-41.1 17.3l-29.1-29.1-54.3 54.3 29.8 29.7c-7.2 12.5-12.6 26.1-16.1 40.5H64v76.8h44.1c3.8 13.7 9.5 26.6 16.7 38.6l-31.7 31.7 54.3 54.3 32.3-32.3c11.7 6.8 24.5 11.9 37.9 15.4v46h76.8v-46c13.5-3.5 26.2-8.6 37.9-15.4l32.3 32.3 54.3-54.3-31.6-31.7c7.2-11.9 12.9-24.8 16.7-38.6h44zm-192 15.4c-29.7 0-53.7-24.1-53.7-53.8s24-53.8 53.7-53.8 53.8 24.1 53.8 53.8-24.1 53.8-53.8 53.8z');
-const iconNo = icon('M443.6 387.1L312.4 255.4l131.5-130c5.4-5.4 5.4-14.2 0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L256 197.8 124.9 68.3c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L68 105.9c-5.4 5.4-5.4 14.2 0 19.6l131.5 130L68.4 387.1c-2.6 2.6-4.1 6.1-4.1 9.8 0 3.7 1.4 7.2 4.1 9.8l37.4 37.6c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1L256 313.1l130.7 131.1c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1l37.4-37.6c2.6-2.6 4.1-6.1 4.1-9.8-.1-3.6-1.6-7.1-4.2-9.7z');
-const iconOk = icon('M448 71.9c-17.3-13.4-41.5-9.3-54.1 9.1L214 344.2l-99.1-107.3c-14.6-16.6-39.1-17.4-54.7-1.8-15.6 15.5-16.4 41.6-1.7 58.1 0 0 120.4 133.6 137.7 147 17.3 13.4 41.5 9.3 54.1-9.1l206.3-301.7c12.6-18.5 8.7-44.2-8.6-57.5z');
-const iconRemove = icon('M417.4 224H94.6C77.7 224 64 238.3 64 256s13.7 32 30.6 32h322.8c16.9 0 30.6-14.3 30.6-32s-13.7-32-30.6-32z');
+const iconAdd = icon(512, 'M417.4 224H288V94.6c0-16.9-14.3-30.6-32-30.6s-32 13.7-32 30.6V224H94.6C77.7 224 64 238.3 64 256s13.7 32 30.6 32H224v129.4c0 16.9 14.3 30.6 32 30.6s32-13.7 32-30.6V288h129.4c16.9 0 30.6-14.3 30.6-32s-13.7-32-30.6-32z');
+const iconDo = icon(128, 'M36.108 110.473l70.436-70.436-18.581-18.58-70.437 70.436c-.378.302-.671.716-.803 1.22l-5.476 20.803c-.01.04-.01.082-.019.121-.018.082-.029.162-.039.247-.007.075-.009.147-.009.222-.001.077.001.147.009.225.01.084.021.166.039.246.008.04.008.082.019.121.007.029.021.055.031.083.023.078.053.154.086.23.029.067.057.134.09.196.037.066.077.127.121.189.041.063.083.126.13.184.047.059.1.109.152.162.053.054.105.105.163.152.056.048.119.09.182.131.063.043.124.084.192.12.062.033.128.062.195.09.076.033.151.063.23.087.028.009.054.023.083.031.04.01.081.01.121.02.081.017.162.028.246.037.077.009.148.011.224.01.075 0 .147-.001.223-.008.084-.011.166-.022.247-.039.04-.01.082-.01.121-.02l20.804-5.475c.505-.132.92-.425 1.22-.805zm-16.457-2.124c-.535-.535-1.267-.746-1.964-.649l3.183-12.094 11.526 11.525-12.096 3.182c.098-.697-.112-1.429-.649-1.964zm90.051-71.47l-18.58-18.581 7.117-7.117s12.656 4.514 18.58 18.582l-7.117 7.116z');
+const iconNo = icon(512, 'M443.6 387.1L312.4 255.4l131.5-130c5.4-5.4 5.4-14.2 0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L256 197.8 124.9 68.3c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L68 105.9c-5.4 5.4-5.4 14.2 0 19.6l131.5 130L68.4 387.1c-2.6 2.6-4.1 6.1-4.1 9.8 0 3.7 1.4 7.2 4.1 9.8l37.4 37.6c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1L256 313.1l130.7 131.1c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1l37.4-37.6c2.6-2.6 4.1-6.1 4.1-9.8-.1-3.6-1.6-7.1-4.2-9.7z');
+const iconOk = icon(512, 'M448 71.9c-17.3-13.4-41.5-9.3-54.1 9.1L214 344.2l-99.1-107.3c-14.6-16.6-39.1-17.4-54.7-1.8-15.6 15.5-16.4 41.6-1.7 58.1 0 0 120.4 133.6 137.7 147 17.3 13.4 41.5 9.3 54.1-9.1l206.3-301.7c12.6-18.5 8.7-44.2-8.6-57.5z');
+const iconRemove = icon(512, 'M417.4 224H94.6C77.7 224 64 238.3 64 256s13.7 32 30.6 32h322.8c16.9 0 30.6-14.3 30.6-32s-13.7-32-30.6-32z');
 /* eslint-enable max-len */
 
 class PureComponent extends Component {
@@ -63,25 +63,29 @@ class Account extends PureComponent {
                 )}
 
                 {!!props.user && (
-                    <button className="w-100" onClick={onLogout}>Log Out</button>
+                    <button className="w-100" onClick={onLogout} title="Log Out">
+                        Log Out
+                    </button>
                 )}
 
                 {!!props.user || (
-                    <label className="flex mb1" htmlFor="email">
+                    <label className="flex mb1" htmlFor="email" title="Email">
                         <code><b>L</b></code>
                         <input className={`bg-${props.error ? 'washed-red' : 'near-white'} bw0 flex-auto ml1 ph1`} defaultValue="admin@doctear.com" id="email" name="email" ref={this.onRefEmail} type="email" />
                     </label>
                 )}
 
                 {!!props.user || (
-                    <label className="flex mb1" htmlFor="password">
+                    <label className="flex mb1" htmlFor="password" title="Password">
                         <code><b>P</b></code>
                         <input className={`bg-${props.error ? 'washed-red' : 'near-white'} bw0 flex-auto ml1 ph1`} defaultValue="doctear" id="password" name="password" ref={this.onRefPassword} type="password" />
                     </label>
                 )}
 
                 {!!props.user || (
-                    <button className="w-100">Log In</button>
+                    <button className="w-100" title="Log In">
+                        Log In
+                    </button>
                 )}
             </form>
         );
@@ -198,7 +202,7 @@ class Header extends PureComponent {
     render () {
         return (
             <header className="b--dark-gray bb bw1 cf pt1">
-                <a href="/">
+                <a className="dark-gray hover-gray link" href="/">
                     <svg className="fl w3" viewBox="0 0 32 32">
                         <path d="M7 21v4h1v-4zm10 0v4h1v-4" />
                         <path d="M9 21v3h1v-3zm16 0v3h1v-3" />
@@ -208,7 +212,7 @@ class Header extends PureComponent {
                         <path d="M27 20H6v2H5v-5h1v2h21v-2h1v5h-1v-2zm-1-2v-8l-6-7H9C7 3 7 4 7 5v13h1V5c0-1 0-1 1-1h10v5c0 1 1 2 2 2h4v7z" />
                     </svg>
 
-                    <h1 className="dark-gray fl f4 hover-gray link">
+                    <h1 className="fl f4">
                         DocTear
                     </h1>
                 </a>
@@ -296,23 +300,23 @@ class Viewer extends PureComponent {
     render (props) {
         return (
             <div className="bottom-1 fixed right-1">
-                <div className="b--dark-gray ba bg-white br-100 bw1 cf h2 hover-dark-pink link mb1 pointer tc w2" onClick={onAdd}>
+                <div className={getButtonClasses('dark-pink')} onClick={onAdd} title="Create">
                     {iconAdd}
                 </div>
 
                 {props.view || props.proof && (
-                    <div className="b--dark-gray ba bg-white br-100 bw1 cf h2 hover-red link mb1 pointer tc w2" onClick={onRemove}>
+                    <div className={getButtonClasses('red')} onClick={onRemove} title="Remove">
                         {iconRemove}
                     </div>
                 )}
 
                 {props.view || (
-                    <div className="b--dark-gray ba bg-white br-100 bw1 cf h2 hover-green link mb1 pointer tc w2" onClick={onSave}>
+                    <div className={getButtonClasses('green')} onClick={onSave} title="Save">
                         {iconOk}
                     </div>
                 )}
 
-                <div className={`b--dark-gray ba bg-white br-100 bw1 cf h2 hover-${props.view ? 'dark-blue' : 'blue'} link pointer tc w2`} onClick={onView}>
+                <div className={getButtonClasses(props.view ? 'dark-blue' : 'blue')} onClick={onView} title={props.view ? 'Edit' : 'Cancel'}>
                     {props.view ? iconDo : iconNo}
                 </div>
             </div>
@@ -360,18 +364,22 @@ export class Application extends Component {
                     <Proofs proofs={state.proofs} search={state.search} />
                 )}
 
-                {!!state.user && (
-                    <Viewer proof={!!state.proof} view={state.view} />
-                )}
-
                 {!!state.user && state.proof ? (
                     <Proof labels={state.labels} proof={state.proof} view={state.view} />
                 ) : (
                     <Description className={state.user ? 'w-50' : 'w-75'} />
                 )}
+
+                {!!state.user && (
+                    <Viewer proof={!!state.proof} view={state.view} />
+                )}
             </main>
         );
     }
+}
+
+function getButtonClasses (color) {
+    return `b--dark-gray ba bg-white br-100 bw1 cf h2 hover-${color} link mb1 pointer tc w2`;
 }
 
 function getContent (disabled, html, placeholder) {
@@ -391,10 +399,10 @@ function getProofColor (proof) {
     ;
 }
 
-function icon (d) {
+function icon (size, d) {
     return (
-        <svg className="ma1" viewBox="0 0 512 512">
-            <path fill="currentColor" fillRule="evenodd" d={d} />
+        <svg className="ma1" viewBox={`0 0 ${size} ${size}`}>
+            <path d={d} />
         </svg>
     );
 }
