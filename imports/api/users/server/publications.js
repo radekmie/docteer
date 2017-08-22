426515fd7753d1b6a6726561e3fa49e8d422f0cc
Radosław Miernik <radekmie@gmail.com>
2017-08-22 19:45:04 +0200

    Removed extra account.

--- imports/api/users/server/publications.js
+++ imports/api/users/server/publications.js
@@ -1,9 +1,4 @@
-import {Accounts} from 'meteor/accounts-base';
-import {Meteor}   from 'meteor/meteor';
-
-if (Meteor.users.find({}, {fields: {_id: 1}}).count() === 0) {
-    Accounts.createUser({email: 'admin@doctear.com', password: 'doctear'});
-}
+import {Meteor} from 'meteor/meteor';
 
 Meteor.publish('users.self', function publishUsersSelf () {
     return Meteor.users.find({_id: this.userId}, {fields: {emails: 1, schemas: 1}});
