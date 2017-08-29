2e6981328ff87106e7b7355750f6979d44d869a3
Radosław Miernik <radekmie@gmail.com>
2017-08-29 22:43:45 +0200

    Bulk writes.

--- imports/api/notes/server/graphql.js
+++ imports/api/notes/server/graphql.js
@@ -17,67 +17,66 @@ function bulkPatch (collection, patch, userId) {
   const now = new Date();
 
   const hand = collection.rawCollection();
-  const bulk = hand.initializeOrderedBulkOp();
+  const bulk = [];
 
-  patch.removed.forEach(_id => bulk.find({_id_slug: _id, _id_user: userId}).updateOne({$set: {_removed: now, _updated: now}}));
+  patch.removed.forEach(_id => {
+    bulk.push({
+      updateOne: {
+        filter: {_id_slug: _id, _id_user: userId},
+        update: {$set: {_removed: now, _updated: now}}
+      }
+    });
+  });
+
   patch.updated.forEach(doc => {
     const _id      = doc._id;
     const _outline = doc._outline;
 
-    if (patch.removed.includes(_id)) {
+    if (patch.removed.includes(_id))
       return;
-    }
 
     delete doc._id;
     delete doc._outline;
 
-    const keys = Object.keys(doc);
-
-    if (keys.length === 0) {
+    if (Object.keys(doc).length === 0)
       return;
-    }
 
     if (patch.created.includes(_id)) {
-      bulk.insert({
-        // ID
-        _id_slug: _id,
-        _id_user: userId,
+      bulk.push({
+        insertOne: {
+          // ID
+          _id_slug: _id,
+          _id_user: userId,
 
-        // Type
-        _outline,
+          // Type
+          _outline,
 
-        // Dates
-        _created: now,
-        _removed: null,
-        _updated: now,
+          // Dates
+          _created: now,
+          _removed: null,
+          _updated: now,
 
-        // History
-        _version: [{_created: now, ...doc}],
+          // History
+          _version: [{_created: now, ...doc}],
 
-        // Data
-        ...doc
+          // Data
+          ...doc
+        }
       });
     } else {
-      // NOTE: Atomic update would be the best but there's an security issue.
-      // bulk.find({_id_slug: _id, _id_user: userId}).updateOne({
-      //     $set:  {_updated: now, ...doc},
-      //     $push: {_version: {_created: now, ...doc}}
-      // });
-
-      bulk.find({_id_slug: _id, _id_user: userId}).updateOne({
-        $set:             {_updated: now},
-        $push: {_version: {_created: now}}
-      });
-
-      keys.forEach(key => {
-        bulk.find({_id_slug: _id, _id_user: userId, [key]: {$exists: true}, '_version._created': now}).updateOne({
-          $set: {[key]: doc[key], [`_version.$.${key}`]: doc[key]}
-        });
+      bulk.push({
+        updateOne: {
+          filter: {_id_slug: _id, _id_user: userId},
+          update: {
+            $set:             {_updated: now},
+            $push: {_version: {_created: now, ...doc}}
+          }
+        }
       });
     }
   });
 
-  return bulk.execute();
+  return hand.bulkWrite(bulk);
 }
 
 const GraphQLDate = new GraphQLScalarType({
