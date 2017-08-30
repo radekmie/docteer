997392f16b5d3ce1c837c50b0b98d08f89fb0968
Radosław Miernik <radekmie@gmail.com>
2017-08-30 21:21:18 +0200

    Production deploy.

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -1,43 +1,52 @@
 module.exports = {
+  app: {
+    name: 'docteer',
+    path: '..',
+
     servers: {
-        one: {
-            host: '46.101.200.204',
-            username: 'root'
-        }
+      first: {}
     },
 
-    app: {
-        name: 'docteer',
-        path: '..',
-
-        servers: {
-            one: {}
-        },
-
-        buildOptions: {
-            serverOnly: true
-        },
-
-        env: {
-            MONGO_URL: 'mongodb://localhost/meteor',
-            ROOT_URL: 'http://46.101.200.204'
-        },
-
-        docker: {
-            image: 'abernix/meteord:node-8.3.0-base',
-            imagePort: 80
-        },
-
-        deployCheckPort: 80,
-        deployCheckWaitTime: 60,
-
-        enableUploadProgressBar: true
+    buildOptions: {
+      serverOnly: true
     },
 
-    mongo: {
-        version: '3.4.1',
-        servers: {
-            one: {}
-        }
+    env: {
+      MONGO_URL: 'mongodb://localhost/meteor',
+      ROOT_URL: 'https://docteer.com'
+    },
+
+    docker: {
+      image: 'abernix/meteord:node-8.3.0-base',
+      imagePort: 80
+    },
+
+    deployCheckPort: 80,
+    deployCheckWaitTime: 60,
+
+    enableUploadProgressBar: true
+  },
+
+  mongo: {
+    version: '3.4.1',
+    servers: {
+      first: {}
     }
+  },
+
+  proxy: {
+    domains: 'docteer.com,www.docteer.com',
+
+    ssl: {
+      forceSSL: true,
+      letsEncryptEmail: 'radekmie@gmail.com'
+    }
+  },
+
+  servers: {
+    first: {
+      host: '46.101.200.204',
+      username: 'root'
+    }
+  }
 };
