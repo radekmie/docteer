49def6ab90ca452d6d8afe5ce6a7cbbc3457fd9e
Radosław Miernik <radekmie@gmail.com>
2017-09-01 21:17:37 +0200

    Configuration fixes.

--- imports/server/optimizeStatics.js
+++ imports/server/optimizeStatics.js
@@ -6,7 +6,7 @@ export const optimizeOptions = {allowed: ['asset', 'js']};
 export const optimize = () => {
   // NOTE: This package requires manifest.json.
   // eslint-disable-next-line no-undef
-  if (window.Package['bundle-visualizer'])
+  if (global.Package['bundle-visualizer'])
     return;
 
   for (const path in WebAppInternals.staticFiles)
