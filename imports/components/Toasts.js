9b40aa630d2ccb43f0cba02e2f9a83e1d6504858
Radosław Miernik <radekmie@gmail.com>
2017-09-07 14:40:21 +0200

    Toasts tweaks.

--- imports/components/Toasts.js
+++ imports/components/Toasts.js
@@ -17,9 +17,9 @@ type Toasts$Props = {
 
 export function Toasts (props: Toasts$Props) {
   return (
-    <div class="bottom-1 center-h fixed hide-child">
+    <div class="bottom-1 center-h fixed hide-child w5">
       {props.toasts.map(toast =>
-        <div class={`bl bg-near-white bw2 b--dark-${COLOR[toast.type]}${toast.dead ? ' child' : ''} mt1 pa1 ph2 shadow-4 tj w5`} key={toast._id}>
+        <div class={`bl bg-near-white bw2 b--dark-${COLOR[toast.type]}${toast.dead ? ' child' : ''} mt1 pa1 ph2 shadow-4 tj`} key={toast._id}>
           {toast.text}
         </div>
       )}
