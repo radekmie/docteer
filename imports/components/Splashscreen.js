8ed05f22132b15c0df5ee7115be6fbe9e24cccd6
Radosław Miernik <radekmie@gmail.com>
2017-09-07 21:19:56 +0200

    Added note about demo account.

--- imports/components/Splashscreen.js
+++ imports/components/Splashscreen.js
@@ -6,7 +6,7 @@ import {h} from 'preact';
 export function Splashscreen () {
   return (
     <div class="flex-1 pt3 tc">
-      Some description should be placed here!
+      There's no description nor introduction yet, but there's an demo account: <code>demo@docteer.com:demo</code>. It has access to all features, but keep in mind, that changing its password won't do anything.
     </div>
   );
 }
