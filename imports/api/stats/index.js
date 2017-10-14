eafef5fa4d2009b9367b77dc6db894e46ff88991
Radosław Miernik <radekmie@gmail.com>
2017-10-14 18:58:16 +0200

    Removed stats.

--- imports/api/stats/index.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-import {Mongo} from 'meteor/mongo';
-
-export const Stats = new Mongo.Collection('stats');
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 // @flow
 
 import '../imports/api/notes/server';
-import '../imports/api/stats/server';
 import '../imports/api/users/server';
 import './imports/optimize';
 import './imports/privacy';
