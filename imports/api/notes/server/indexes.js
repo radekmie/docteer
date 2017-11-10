cab6197cc5caddf3cbe243d382c31f73f6269938
Radosław Miernik <radekmie@gmail.com>
2017-11-10 23:44:18 +0100

    Fixed test startup.

--- imports/api/notes/server/indexes.js
+++ imports/api/notes/server/indexes.js
@@ -1,7 +1,11 @@
 // @flow
 
+import {Meteor} from 'meteor/meteor';
+
 import {Notes} from '..';
 
-Notes._ensureIndex({_id_user: 1, _id_slug: 1}, {unique: true});
-Notes._ensureIndex({_id_user: 1, _updated: 1});
-Notes._ensureIndex({_removed: 1});
+Meteor.startup(() => {
+  Notes._ensureIndex({_id_user: 1, _id_slug: 1}, {unique: true});
+  Notes._ensureIndex({_id_user: 1, _updated: 1});
+  Notes._ensureIndex({_removed: 1});
+});
