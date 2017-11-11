294f7130f5953e7f584807a69ee77d9952029a63
Radosław Miernik <radekmie@gmail.com>
2017-11-11 23:44:02 +0100

    Implemented import merger.

---
+++ server/imports/babel.js
@@ -0,0 +1,42 @@
+module.exports = function transform() {
+  const seen = new Map();
+
+  return {
+    visitor: {
+      // Transforms:
+      //   import {a} from 'x';
+      //   import {b} from 'x';
+      // To:
+      //   import {a, b} from 'x';
+      ImportDeclaration(path) {
+        const file = path.node.source.value;
+
+        if (seen.has(file)) {
+          const prev = seen.get(file);
+
+          path.node.specifiers.push(...prev.node.specifiers);
+          prev.remove();
+        }
+
+        seen.set(file, path);
+      },
+
+      // Transforms:
+      //   <p data-test-x="1" />
+      // To:
+      //   <p />
+      JSXAttribute(path) {
+        if (
+          path.node.name.type === 'JSXIdentifier' &&
+          path.node.name.name.startsWith('data-test')
+        )
+          path.remove();
+      },
+
+      // Reset before each file.
+      Program() {
+        seen.clear();
+      }
+    }
+  };
+};
