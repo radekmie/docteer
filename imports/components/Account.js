81161cda5118292e70914a663a1c1785dc686691
Radosław Miernik <radekmie@gmail.com>
2017-11-22 22:56:23 +0100

    Simplified form submit.

--- imports/components/Account.js
+++ imports/components/Account.js
@@ -35,9 +35,8 @@ export class Account extends Component<Account$Props> {
     event.preventDefault();
 
     if (this.email && this.password) {
-      if (this.props.register)
-        onRegister(this.email.value, this.password.value).catch(() => {});
-      else onLogin(this.email.value, this.password.value).catch(() => {});
+      const action = this.props.register ? onRegister : onLogin;
+      action(this.email.value, this.password.value).catch(() => {});
     }
   };
 
