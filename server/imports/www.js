30e9eec2c0f54d2bc702f42983ebcfd5988598a8
Radosław Miernik <radekmie@gmail.com>
2017-11-26 10:33:26 +0100

    Fixed www to non-www redirect.

--- server/imports/www.js
+++ server/imports/www.js
@@ -8,10 +8,10 @@ WebApp.rawConnectHandlers.stack.unshift({
     const host = req.headers.host || '';
 
     if (host.startsWith('www.')) {
-      res.setHeader(
-        'location',
-        `${req.protocol}://${host.slice(4)}${req.originalUrl}`
-      );
+      res.writeHead(301, {
+        location: `https://${host.slice(4)}${req.originalUrl}`
+      });
+      res.end();
     } else {
       next();
     }
