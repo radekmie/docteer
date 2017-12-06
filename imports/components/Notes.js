fef8ff2f1fc0efa2f360fa9c100a1e296446dde3
Radosław Miernik <radekmie@gmail.com>
2017-12-06 20:31:15 +0100

    Fixed few ESLint problems.

--- imports/components/Notes.js
+++ imports/components/Notes.js
@@ -23,9 +23,11 @@ function Note({note}: Note$Props) {
 
   return (
     <a
-      class={`${note._active ? 'bg-near-white bl bw2 b--dark-gray ' : ''}db ${
-        color
-      } hover-bg-near-white link ph2${note._active ? ' pl1' : ''} truncate`}
+      class={`${
+        note._active ? 'bg-near-white bl bw2 b--dark-gray ' : ''
+      }db ${color} hover-bg-near-white link ph2${
+        note._active ? ' pl1' : ''
+      } truncate`}
       data-test-note={note.name || '(untitled)'}
       dangerouslySetInnerHTML={{__html: note.name || '(untitled)'}}
       href={note._href}
