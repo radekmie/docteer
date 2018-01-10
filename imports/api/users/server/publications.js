d6a198d004f380d4dde037c422c8523c577cf28d
Radosław Miernik <radekmie@gmail.com>
2018-01-10 17:32:45 +0100

    Added short-circuit for publication.

--- imports/api/users/server/publications.js
+++ imports/api/users/server/publications.js
@@ -3,6 +3,8 @@
 import {Meteor} from 'meteor/meteor';
 
 Meteor.publish('LIVE /users/self', function publishUsersSelf() {
+  if (!this.userId) return this.ready();
+
   return Meteor.users.find(
     {_id: this.userId},
     {fields: {emails: 1, schemas: 1}}
