130bcdfe0dbfcde8be7781bd704dc2324435dbb1
Radosław Miernik <radekmie@gmail.com>
2018-02-23 22:04:37 +0100

    Updated packages.

--- server/imports/apm.js
+++ server/imports/apm.js
@@ -7,3 +7,10 @@ const kadira = Meteor.settings.kadira;
 
 if (kadira && kadira.appId && kadira.appSecret)
   Kadira.connect(kadira.appId, kadira.appSecret, kadira.options || {});
+
+// Silent Kadira warnings.
+const endLastEvent = Kadira.Tracer.prototype.endLastEvent;
+Kadira.Tracer.prototype.endLastEvent = function () {
+  endLastEvent.apply(this, arguments);
+  return false;
+};
