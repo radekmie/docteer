b57efa4634a5d18c659cf4033289a2101951744c
Radosław Miernik <radekmie@gmail.com>
2018-08-21 21:34:35 +0200

    Disabled React devtools on production.

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 // @flow
 
-import 'preact/devtools';
-
 import './imports/render';
 import './imports/triggers';
 import './imports/wysiwyg';
+
+if (process.env.NODE_ENV === 'development') require('preact/devtools');
