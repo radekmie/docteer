ae3f0e0026d2db91d304545cacb776373f321773
Radosław Miernik <radekmie@gmail.com>
2018-10-19 09:28:50 +0200

    Removed www redirect.

--- server/index.js
+++ server/index.js
@@ -8,12 +8,10 @@ import serve from 'serve-static';
 import config from '@server/config';
 import {server as api} from '@server/api';
 import {server as ssr} from '@server/ssr';
-import {server as www} from '@server/www';
 
 export const server = connect();
 export const root = join(__dirname, '..');
 
-server.use('/', www);
 server.use('/', compression());
 server.use('/', serve(join(root, 'public'), config.server.static.public));
 server.use('/', serve(join(root, 'client'), config.server.static.client));
