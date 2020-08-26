e0a5b479a0e2d528500ca7f9fd09d575b9b6045d
Radosław Miernik <radekmie@gmail.com>
2020-08-26 21:38:09 +0200

    Synchronized SSR response.

--- server/server/ssr/index.ts
+++ server/server/ssr/index.ts
@@ -3,10 +3,9 @@ import { FastifyInstance } from 'fastify';
 import { params, render } from './_render';
 
 export async function plugin(server: FastifyInstance) {
-  server.get('/*', async (request, reply) => {
+  server.get('/*', (request, reply) => {
     const { body, headers } = render(params(request.url));
     const code = request.headers['if-none-match'] === headers.etag ? 304 : 200;
-    reply.code(code).headers(headers);
-    return body;
+    reply.code(code).headers(headers).send(body);
   });
 }
